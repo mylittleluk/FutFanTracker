c8c67a74f757f4dfd4be3f7e586c3005b3c06080
Lucas Silva <lucasjprn@hotmail.com>
2025-03-08 22:23:57 -0400

    Adaptação do Spinner para uso de String Array no res. Spinner Mode mudado para Dialog ao invés de Dropdown.

--- app/src/main/java/br/edu/utfpr/futfantracker/CadastrarPartidaActivity.java
+++ app/src/main/java/br/edu/utfpr/futfantracker/CadastrarPartidaActivity.java
@@ -2,15 +2,17 @@ package br.edu.utfpr.futfantracker;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+//import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
+//import java.util.ArrayList;
 import java.util.Date;
 
 public class CadastrarPartidaActivity extends AppCompatActivity {
@@ -18,7 +20,7 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
     private EditText editTextData, editTextHorario, editTextAdversario;
     private RadioGroup radioGroupLocal;
     private CheckBox checkBoxAcompanheiPartida;
-    private Button buttonSalvar, buttonLimpar;
+    private Spinner spinnerCompeticao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +32,26 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
         editTextAdversario = findViewById(R.id.editTextAdversario);
         radioGroupLocal = findViewById(R.id.radioGroupLocal);
         checkBoxAcompanheiPartida = findViewById(R.id.checkBoxAcompanheiPartida);
-        buttonSalvar = findViewById(R.id.buttonSalvar);
-        buttonLimpar = findViewById(R.id.buttonLimpar);
+        spinnerCompeticao = findViewById(R.id.spinnerCompeticao);
+
+        //popularSpinner();
+
     }
+
+//    private void popularSpinner(){
+//        ArrayList<String> competicoes = new ArrayList<>();
+//        competicoes.add(getString(R.string.campeonato_estadual));
+//        competicoes.add(getString(R.string.campeonato_brasileiro));
+//        competicoes.add(getString(R.string.copa_do_brasil));
+//        competicoes.add(getString(R.string.copa_libertadores));
+//        competicoes.add(getString(R.string.copa_sulamericana));
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                                                         android.R.layout.simple_list_item_1,
+//                                                         competicoes);
+//
+//        spinnerCompeticao.setAdapter(adapter);
+//    }
 
     public void limparCampos(View view){
         editTextData.setText(null);
@@ -41,6 +60,7 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
         radioGroupLocal.clearCheck();
         checkBoxAcompanheiPartida.setChecked(false);
         editTextData.requestFocus();
+        spinnerCompeticao.setSelection(0);
 
         Toast.makeText(this,
                        getString(R.string.os_campos_foram_limpos),
@@ -55,17 +75,6 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
         Date horarioValidado;
         String dataFormatada;
         String horarioFormatado;
-
-        // Validação Adversário
-        if(adversario == null || adversario.trim().isEmpty()){
-            Toast.makeText(this,
-                           R.string.por_favor_preencha_o_adversario,
-                           Toast.LENGTH_LONG).show();
-            editTextAdversario.requestFocus();
-            return;
-        }
-
-        adversario = adversario.trim();
 
         // Validação Data
         SimpleDateFormat padraoEntradaData = new SimpleDateFormat("dd/MM/yyyy");
@@ -98,8 +107,16 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
             return;
         }
 
-        // Checagem do Estado do Checkbox
-        boolean acompanheiPartida = checkBoxAcompanheiPartida.isChecked();
+        // Validação Adversário
+        if(adversario == null || adversario.trim().isEmpty()){
+            Toast.makeText(this,
+                    R.string.por_favor_preencha_o_adversario,
+                    Toast.LENGTH_LONG).show();
+            editTextAdversario.requestFocus();
+            return;
+        }
+
+        adversario = adversario.trim();
 
         String localPartida;
         int radioButtonId = radioGroupLocal.getCheckedRadioButtonId();
@@ -116,12 +133,26 @@ public class CadastrarPartidaActivity extends AppCompatActivity {
             return;
         }
 
+        // Check Spinner competições
+        String competicao = (String) spinnerCompeticao.getSelectedItem();
+        if(competicao == null){
+            Toast.makeText(this,
+                    R.string.o_spinner_competicoes_nao_possui_valores,
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Checagem do Estado do Checkbox
+        boolean acompanheiPartida = checkBoxAcompanheiPartida.isChecked();
+
+
         Toast.makeText(this,
                        getString(R.string.data_valor)+dataFormatada+"\n"
                        +getString(R.string.horario_valor)+horarioFormatado+"\n"
                        +(acompanheiPartida ? getString(R.string.acompanhei_a_partida) : getString(R.string.nao_acompanhei_a_partida))+"\n"
                        +getString(R.string.adversario_valor)+adversario+"\n"
-                       +getString(R.string.local_valor)+localPartida,
+                       +getString(R.string.local_valor)+localPartida+"\n"
+                       +getString(R.string.competicao_valor)+competicao,
                        Toast.LENGTH_LONG).show();
     }
 }
